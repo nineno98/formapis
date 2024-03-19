fe88059ab22faf49d7082fc5ee3a73401f9d9129
2023450 <2023450@B001-15>
2024-03-19 16:01:35 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using ConsoleApp1;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string endPointUrl = "https://retoolapi.dev/KqpqJ9/data";
+
+        static List<Adat> adatok = new List<Adat>();
+
+        static void Main(string[] args)
+        {
+            restapiAdatok().Wait();
+            foreach (Adat adat in adatok)
+            {
+                Console.WriteLine($"{adat.Id}. {adat.Name}, {adat.Salary}");
+            }
+
+            // 1.feladat
+
+            Console.WriteLine(adatok.Select(x => x.Name));
+            Console.WriteLine("Program vége");
+            Console.ReadKey();
+        }
+
+        
+
+        static async Task restapiAdatok()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, endPointUrl);
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            string jsonstring = await response.Content.ReadAsStringAsync();
+            adatok = Adat.FromJson(jsonstring).ToList();
+
+
+        }
+    }
+}
---
+++ formapis/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+
+namespace formapis
+{
+    public partial class Form1 : Form
+    {
+        static string endPointUrl = "https://retoolapi.dev/KqpqJ9/data";
+
+        static List<Adat> adatok = new List<Adat>();
+
+       
+         async Task restapiAdatok()
+         {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, endPointUrl);
+            var response = await client.SendAsync(request);
+
+            if (!response.EnsureSuccessStatusCode())
+            {
+                
+                string jsonstring = await response.Content.ReadAsStringAsync();
+                adatok = Adat.FromJson(jsonstring).ToList();
+            }
+
+            
+
+
+         }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            restapiAdatok();
+            listBox1.Items.AddRange(adatok.ToArray());
+        }
+
+        
+    }
+}
