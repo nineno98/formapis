07ee8425941d64d22f447ebd19374d0a507a3cd9
nineno98 <2023450@B001-15>
2024-04-16 16:56:21 +0200

    kapcsolat létrejött

---
+++ levesek_database_conn/Leves.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace levesek_database_conn
+{
+    class Leves
+    {
+        private string megnevezes_;
+        private int kaloria_;
+        private double feherje_;
+        private double zsir_;
+        private double szenhidrat_;
+        private double hamu_;
+        private double rost_;
+
+        public Leves(string megnevezes_, int kaloria_, double feherje_, double zsir_, double szenhidrat_, double hamu_, double rost_)
+        {
+            this.megnevezes_ = megnevezes_;
+            this.kaloria_ = kaloria_;
+            this.feherje_ = feherje_;
+            this.zsir_ = zsir_;
+            this.szenhidrat_ = szenhidrat_;
+            this.hamu_ = hamu_;
+            this.rost_ = rost_;
+        }
+
+        public string Megnevezes_ { get => megnevezes_; set => megnevezes_ = value; }
+        public int Kaloria { get => kaloria_; set => kaloria_ = value; }
+        public double Feherje { get => feherje_; set => feherje_ = value; }
+        public double Zsir { get => zsir_; set => zsir_ = value; }
+        public double Szenhidrat { get => szenhidrat_; set => szenhidrat_ = value; }
+        public double Hamu { get => hamu_; set => hamu_ = value; }
+        public double Rost { get => rost_; set => rost_ = value; }
+    }
+}
--- levesek_database_conn/Program.cs
+++ levesek_database_conn/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySqlConnector;
+
 
 namespace levesek_database_conn
 {
@@ -10,6 +12,41 @@ namespace levesek_database_conn
     {
         static void Main(string[] args)
         {
+            string sqlStatement, output;
+            MySqlConnection dbConnection;
+
+            MySqlConnectionStringBuilder strBuild = new MySqlConnectionStringBuilder();
+            strBuild.Server = "localhost";
+
+            strBuild.UserID = "root";
+            strBuild.Password = "";
+            strBuild.Database = "levesek";
+
+            dbConnection = new MySqlConnection(strBuild.ToString());
+            sqlStatement = "SELECT version();";
+
+            try
+            {
+                dbConnection.Open();
+
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = dbConnection;
+                command.CommandText = sqlStatement;
+
+                output = command.ExecuteScalar().ToString();
+                Console.WriteLine(output);
+                Console.ReadKey();
+
+                dbConnection.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
 
         }
     }
